d2cb3324710e361cd1b3226eade27bfd079d4f0e
Bloodyaugust <greysonrichey@gmail.com>
2020-07-06 16:30:53 -0700

    Add first pass for SOWeapon

---
+++ Assets/Data/ScriptableObjects/SOWeapon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Weapon", order = 1)]
+public class SOWeapon : ScriptableObject {
+    public int clipSize;
+    public float cooldown;
+    public float reload;
+    public string range;
+}
