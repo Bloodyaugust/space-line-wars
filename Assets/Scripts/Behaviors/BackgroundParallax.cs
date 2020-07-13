f2ab7d8ea8358ee10a87ec170041910e0303c6c8
Bloodyaugust <greysonrichey@gmail.com>
2020-07-13 11:13:07 -0700

    Make the game more prettier

---
+++ Assets/Scripts/Behaviors/BackgroundParallax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour {
+    public float ParallaxFactor;
+    public float Scale;
+    public float StarScale;
+
+    private MaterialPropertyBlock materialBlock;
+    private SpriteRenderer spriteRenderer;
+
+    void SetMaterial(Vector2 offset) {
+        materialBlock.SetVector("_Offset", new Vector4(offset.x, offset.y));
+        materialBlock.SetFloat("_OverallScale", Scale);
+        materialBlock.SetFloat("_StarsScale", StarScale);
+        spriteRenderer.SetPropertyBlock(materialBlock);
+    }
+
+    void Awake() {
+        materialBlock = new MaterialPropertyBlock();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate() {
+        Vector2 newOffset = Camera.main.transform.position * ParallaxFactor;
+
+        SetMaterial(newOffset);
+    }
+}
---
+++ Assets/Scripts/Behaviors/FullscreenSprite.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullscreenSprite : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private Vector2 spriteSize;
+
+    void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteSize = spriteRenderer.sprite.bounds.size;
+    }
+    
+    void LateUpdate() {
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        
+        Vector2 scale = new Vector2(1, 1);
+        if (cameraSize.x >= cameraSize.y) { // Landscape (or equal)
+            scale *= cameraSize.x / spriteSize.x;
+        } else { // Portrait
+            scale *= cameraSize.y / spriteSize.y;
+        }
+
+        transform.localScale = scale;
+    }
+}
