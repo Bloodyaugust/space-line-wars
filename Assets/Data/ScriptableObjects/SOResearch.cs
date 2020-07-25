b40eaec0149023f633cca53b8471b36da02efeeb
Bloodyaugust <greysonrichey@gmail.com>
2020-07-25 14:51:55 -0600

    Add SOResearch for research data

---
+++ Assets/Data/ScriptableObjects/SOResearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Research", order = 1)]
+public class SOResearch : ScriptableObject {
+    public float amount;
+    public float cost;
+    public Texture2D icon;
+    public SOResearch[] prerequisites;
+    public string description;
+    public string key;
+}
