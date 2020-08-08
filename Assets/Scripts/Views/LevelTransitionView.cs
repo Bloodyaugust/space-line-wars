f3c11e0a2fb7f3365cfd500ac09204886a8c1d8f
Bloodyaugust <greysonrichey@gmail.com>
2020-08-08 16:46:10 -0600

    Add level transition on play press

---
+++ Assets/Scripts/Views/LevelTransitionView.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTransitionView : MonoBehaviour {
+    private Animation animation;
+    private RawImage vignette;
+    private RectTransform view;
+    private UIController uiController;
+
+    void Awake() {
+        animation = GetComponent<Animation>();
+        uiController = UIController.Instance;
+        view = GetComponent<RectTransform>();
+        vignette = GetComponent<RawImage>();
+
+        uiController.StoreUpdated += OnStoreUpdated;
+    }
+
+    void Hide() {
+        vignette.enabled = false;
+    }
+
+    void OnTransitionInComplete() {
+        uiController.SetValue("GameState", GameState.Playing);
+    }
+
+    void OnTransitionOutComplete() {
+        Hide();
+    }
+
+    void OnStoreUpdated(string storeKey) {
+        if (storeKey == "GameState") {
+            if (uiController.Store[storeKey] == GameState.Loading) {
+                Show();
+            }
+        }
+    }
+
+    void Show() {
+        vignette.enabled = true;
+        animation.Play();
+    }
+}
