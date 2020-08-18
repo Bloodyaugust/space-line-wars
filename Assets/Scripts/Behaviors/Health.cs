386ca38914107730117d3d182e0d6e347e2529da
Bloodyaugust <greysonrichey@gmail.com>
2020-08-17 20:30:28 -0600

    Add Healthbar for health behavior

--- Assets/Scripts/Behaviors/Health.cs
+++ Assets/Scripts/Behaviors/Health.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class Health : MonoBehaviour {
     public event Action Died;
 
+    public bool EnableHealthbar;
     public float Hitpoints;
+    public Vector3 HealthbarOffset;
 
     private bool isDead = false;
     private float startingHitpoints;
+    private SpriteRenderer healthBackground;
+    private SpriteRenderer healthOverlay;
 
     public void Damage(float amount) {
         if (!isDead) {
@@ -28,9 +32,26 @@ public class Health : MonoBehaviour {
         gameObject.layer = LayerMask.NameToLayer(GetComponentInParent<ITargetable>().Team.ToString());
     }
 
+    void Awake() {
+        healthBackground = transform.Find("HealthBackground").GetComponent<SpriteRenderer>();
+        healthOverlay = transform.Find("HealthOverlay").GetComponent<SpriteRenderer>();
+
+        if (!EnableHealthbar) {
+            healthBackground.enabled = false;
+            healthOverlay.enabled = false;
+        } else {
+            healthBackground.transform.Translate(HealthbarOffset);
+            healthOverlay.transform.Translate(HealthbarOffset);
+        }
+    }
+
     void Update() {
         if (isDead) {
             Died.Invoke();
+        }
+
+        if (EnableHealthbar) {
+            healthOverlay.transform.localScale = new Vector3(Hitpoints / startingHitpoints, 1, 1);
         }
     }
 }
