f751ac9e26adf1f438dd8e23055c91bccdc2e448
Bloodyaugust <greysonrichey@gmail.com>
2020-08-27 02:58:13 -0600

    Add Heavy Cruiser
    - Add new movement type for capital ships
    - Add new Railgun and projectile

--- Assets/Scripts/Behaviors/ShipMove.cs
+++ Assets/Scripts/Behaviors/ShipMove.cs
@@ -56,11 +56,22 @@ public class ShipMove : MonoBehaviour {
 
     void Update() {
         Vector3 movement = Vector3.zero;
+        Vector3 targetDirection;
+        Vector3 rotatedTargetDirection;
+        Quaternion targetRotation;
 
         switch(currentState) {
             case ShipState.Attack:
                 if (currentTarget != null) {
                     switch(ship.ShipData.moveType) {
+                        case "capital":
+                            targetDirection = (navLinePoints[navLineIndex] - transform.position).normalized;
+                            rotatedTargetDirection = Quaternion.Euler(0, 0, 90) * targetDirection;
+                            targetRotation = Quaternion.LookRotation(Vector3.forward, rotatedTargetDirection);
+
+                            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * turnRate);
+                            movement = transform.right * Time.deltaTime * speed;
+                            break;
                         case "fighter":
                             float angleToTarget = Vector2.SignedAngle(transform.right, currentTarget.transform.position - transform.position);
 
@@ -80,18 +91,9 @@ public class ShipMove : MonoBehaviour {
                         case "strafing":
                             angleToTarget = Vector2.SignedAngle(transform.right, currentTarget.transform.position - transform.position);
 
-                            if (Mathf.Abs(angleToTarget) <= 30) {
-                                transform.Rotate(0, 0, turnRate * Time.deltaTime * Mathf.Sign(angleToTarget), Space.Self);
-                                trackingLastFrame = true;
-                            } else {
-                                if (trackingLastFrame) {
-                                    turnDirection = turnDirection > 0 ? -1 : 1;
-                                }
+                            transform.Rotate(0, 0, turnRate * Time.deltaTime * Mathf.Sign(angleToTarget), Space.Self);
 
-                                transform.Rotate(0, 0, turnRate * Time.deltaTime * turnDirection, Space.Self);
-                                trackingLastFrame = false;
-                            }
-                            movement = transform.up * Time.deltaTime * speed * Mathf.Sin(Time.time);
+                            movement = transform.up * Time.deltaTime * speed * Mathf.Sin(Time.time) / 4;
                             break;
                         default:
                             break;
@@ -99,7 +101,7 @@ public class ShipMove : MonoBehaviour {
                 }
                 break;
             case ShipState.Follow:
-                if (Vector3.Distance(transform.position, navLinePoints[navLineIndex]) <= 1) {
+                if (Vector3.Distance(transform.position, navLinePoints[navLineIndex]) <= Mathf.Lerp(2, 0.1f, ship.ShipData.turnRate / 180)) {
                     if (navLineIndex == navLine.positionCount - 1) {
                         FollowComplete?.Invoke();
                     } else {
@@ -107,9 +109,9 @@ public class ShipMove : MonoBehaviour {
                     }
                 }
 
-                Vector3 targetDirection = (navLinePoints[navLineIndex] - transform.position).normalized;
-                Vector3 rotatedTargetDirection = Quaternion.Euler(0, 0, 90) * targetDirection;
-                Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, rotatedTargetDirection);
+                targetDirection = (navLinePoints[navLineIndex] - transform.position).normalized;
+                rotatedTargetDirection = Quaternion.Euler(0, 0, 90) * targetDirection;
+                targetRotation = Quaternion.LookRotation(Vector3.forward, rotatedTargetDirection);
 
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * turnRate);
                 movement = transform.right * Time.deltaTime * speed;
